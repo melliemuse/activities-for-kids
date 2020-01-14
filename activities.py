6ae5c7f524a9ce8931a513dd4b58b7bf908474fe
Melody Stern <melliemuse@gmail.com>
2020-01-14 14:11:21 -0600

    added instructions

---
+++ activities.py
@@ -0,0 +1,10 @@
+# Define four Python functions named run, swing, slide, and hide_and_seek. Each function should take a child's name as an argument. Each function should print that the child performed the activity.
+
+# For example, Jay ran like a fool! or Chantelle slid down the slide!.
+
+# The following lists of children should be iterated, and the names sent to the appropriate functions.
+
+# running_kids = ["Pam", "Sam", "Andrea", "Will"]
+# swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+# sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+# hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"] 
